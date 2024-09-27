b19426536cf20349b65b7757e6683c59ec47ba0c
iamdwn <truongdinhdongduong@gmail.com>
2024-09-27 21:53:31 +0700

    fix: adjust adding task screen

--- src/app/src/main/java/com/mobile/dailytaskmanager/AddTaskActivity.java
+++ src/app/src/main/java/com/mobile/dailytaskmanager/AddTaskActivity.java
@@ -1,24 +1,41 @@
 package com.mobile.dailytaskmanager;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TimePicker;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class AddTaskActivity extends AppCompatActivity {
+
+    private EditText editTextTaskName;
+    private TimePicker timePicker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_add_task);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        editTextTaskName = findViewById(R.id.editTextTaskName);
+        timePicker = findViewById(R.id.timePicker);
+        Button buttonAddTask = findViewById(R.id.buttonAddTask);
+
+        buttonAddTask.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String taskName = editTextTaskName.getText().toString();
+                int hour = timePicker.getHour();
+                int minute = timePicker.getMinute();
+
+                Intent resultIntent = new Intent();
+                resultIntent.putExtra("TASK_NAME", taskName);
+                resultIntent.putExtra("TASK_HOUR", hour);
+                resultIntent.putExtra("TASK_MINUTE", minute);
+                setResult(RESULT_OK, resultIntent);
+                finish();
+            }
         });
     }
 }
